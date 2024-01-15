49bf84d3eae56c2a85ca1a2cdd2e8d8bd4b3b02c
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-01-15 20:04:59 +0300

    lesson-5 da 13ta masala

--- lesson-5/all.js
+++ lesson-5/all.js
@@ -27,3 +27,130 @@
 ////////////////----------- Uy vazifalar -------------//////////////
 
 // 1- misol
+
+// let a = 7;
+// let b = 12;
+// let c = 0
+// for (let i = a; i <= b; i++){
+//     console.log(i);
+//     c++
+// }
+// console.log(c + " chiqarilgan sonlar soni");
+
+//-----------------------------------------------------------------
+
+// 2 -misol
+
+// let a = 7;
+// let b = 12;
+// let c = 0
+// for (let i = b-1; i > a; i--){
+//     console.log(i);
+//     c++
+// }
+// console.log(c + " chiqarilgan sonlar soni");
+
+//-----------------------------------------------------------------------
+
+// 3 - miosl
+// let narh = 10;
+
+// for(let i = 1 ; i < 11; i++){
+//     console.log(`${i} kg konfet narhi ${narh * i}`);
+// }
+
+//------------------------------------------------------------------------
+
+// 4 - misol
+
+// let narh = 10000;
+// for(let i = 1 ; i <= 2; i += 0.2){
+//     console.log(`${i} kg konfet narhi ${narh * i}`);
+// }
+
+//------------------------------------------------------------------------
+
+// 5 - misol
+
+// let a = 1;
+// let b = 10;
+// let c = 0;
+// for(let i = a + 1; i < b ; i++){
+//     c += i;
+// }
+// console.log(`a va b oraligidagi barcha butun sonlarning yigindisi ${c} barobar`);
+
+//--------------------------------------------------------------------------------------
+
+// 7 - misol
+
+// let a = 1;
+// let b = 5;
+// let c = 1;
+// for(let i = a + 1; i < b ; i++){
+//     c *= i;
+// }
+// console.log(`a va b oraligidagi barcha butun sonlarning ko'paytmasi ${c} barobar`);
+
+//-----------------------------------------------------------------------------------------
+
+// 8 -misol
+
+//  let n =10;
+//  let s =0;
+
+//  for(let i = 1; i<=n; i++){
+//     s += 1/i
+//  }
+//  console.log(s);
+
+// -------------------------------------------------------------------------------
+
+// 9 -misol
+
+//  let n =2;
+//  let s =1;
+
+//  for(let i = 1.1; i<=n; i += 0.1){
+//     //console.log(+i.toFixed(1));
+//     s *= i
+//  }
+//  console.log(s);
+
+//----------------------------------------------------------------------------
+
+//11 -misol 
+
+// let n = 5;
+// let s = 0;
+// for (let i=1; i<=(2*n-1) ; i +=2){
+//     s += i;
+//     console.log(s)
+// }
+// console.log(`${n} ning kivadrati ${s} ga teng`);
+
+//-----------------------------------------------------------------
+
+// 12- misol
+
+// let n = 5;
+// let a = 2;
+// let c = 1;
+// for(let i = 1; i <= n; i++ ){
+//     c*=a;
+// }
+// console.log(`${a} soninig ${n} darajasi teng ${c} ga`);
+
+//------------------------------------------------------------------------
+
+// 13- misol
+
+// let n = 5;
+// let a = 2;
+// for(let i = 1; i <= n; i++ ){
+//     console.log(`${a} ning ${i} darajasi teng `+ a ** i + " ga");
+// }
+
+//-----------------------------------------------------------------
+
+// 14 - misol
