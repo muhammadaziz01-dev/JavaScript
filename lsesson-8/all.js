8ab8a29b06effc08a60063578c71e533edd88652
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-01-18 11:17:45 +0300

    lesson-8

---
+++ lsesson-8/all.js
@@ -0,0 +1,14 @@
+// lesson -8 
+// Uyvazifa funcsiyda 10 ta masala
+
+// 1 - misol
+
+// function sonKub(n) {
+//   return n ** 3;
+// }
+
+// console.log(sonKub(3));
+
+//------------------------------------------
+
+// 2 - misol 
