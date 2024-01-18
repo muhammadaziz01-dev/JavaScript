ea183756f569523e5f04d33a2430cc59013443fd
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-01-18 14:56:39 +0300

    uy vazifa n=misollar tugadi va redme

--- lsesson-8/all.js
+++ lsesson-8/all.js
@@ -12,3 +12,123 @@
 //------------------------------------------
 
 // 2 - misol 
+
+// function sonAyrma(a , b) {
+//     return a - b ;
+//   }
+  
+//   console.log(sonAyrma(3 , 1));
+
+// ----------------------------------------------
+
+//3 - misol
+
+// function sonQoshilma(a , b) {
+//     return a + b ;
+//   }
+  
+//   console.log(sonQoshilma(3 , 7));
+
+//----------------------------------------------------
+
+// 4 - misol 
+
+// function sonDaraja(n) {
+//     console.log(n**2);
+//     console.log(n**3);
+//     console.log(n**4);  
+// }
+
+// sonDaraja(2);
+
+//-----------------------------------------
+
+// 5 - misol
+
+// function sonToq(n) {
+//     for (let i = 0; i < n; i++) {
+//         if (i % 2 == 1) {
+//             console.log(i);
+//         } 
+        
+//     }
+// }
+// sonToq(10);
+
+//----------------------------------------------
+
+// 6 - misol
+
+// function sonJuft(n) {
+//     for (let i = 0; i < n; i++) {
+//         if (i % 2 == 0) {
+//             console.log(i);
+//         } 
+        
+//     }
+// }
+// sonJuft(10);
+
+// -----------------------------------------------------
+
+// 7 - misol
+
+// function sonDinInc(n){
+//     console.log(--n);
+//     console.log(++n);
+// }
+
+// sonDinInc(7);
+
+//-------------------------------------------------------
+
+// 8 - misol
+
+// function sonBoluvch(n) {
+//     for (let i = 1 ; i < n ; i++) {
+//         if(n % i == 0){
+//             console.log(i);
+//         }
+//     }
+// }
+// sonBoluvch(10);
+
+// --------------------------------------------------------
+
+// 9 - midol
+
+// function amalar(a , b , c) {
+//     switch (c){
+//         case "+":
+//             return a + b ;
+//         case "-":
+//             return a - b ;
+//         case "*":
+//             return a * b ;
+//         case "/":
+//             return a / b ;  
+//         case "%":
+//             return a % b ;
+//         default:
+//             return "Invalit operator"
+//     }
+    
+// }
+
+// console.log(amalar(7, 5, "*"));
+
+// --------------------------------------------------
+
+// 10 - misol
+
+// function kara(n) {
+//     for(let i = 0; i <= n ; i++){
+//         for(let j = 1 ; j <= 10; j++ ){
+//             console.log(`${i} x ${j} = ${i*j}`)
+//         }
+//     }
+// }
+
+// kara(5);
+
+//-------------------------------------------------------
---
+++ lsesson-8/redme.md
@@ -0,0 +1,33 @@
+# **Lesson-8** <p>*Mavzu:* **Funcsiya , funcsiyanig asosiy 3 ta turi:**</p> <p>*- Function declaration*</p> <p>*- Function expession*</p> <p>*- Arrow function*</p>
+
+## 1.  F U N C T I O N S 
+ -  *Funksiya - JavaScript dasturlash tilining asoslaridan biri bo'lob uning yordamida malum bir vazifani bajarish mumkun . Funksiya boshqa bir kod qismida chaqirilganda ishga tushadi . Funksiya yordamida kodni qayta ishlatishlik imkoni mavjud yani birmarotaba elon qilib , bir necha joyda ishlatishlik imkoni mavjud , o'hshati lozim bo'lsa SCSS degi mixsend larga qisman o'hshab ketadi .*
+
+<p>Sintactisti quydagicha :</p>
+
+ ```
+ function square(number) {
+  return number * number;
+}
+
+ ```
+
+<p>M:</p>
+
+```
+function power(a,n){
+    let b = a**n;
+    return b
+}
+
+console.log(power(3,2));
+
+// natija ==> 9 
+```
+### *Function declaration*
+- *Funktsiya deklaratsiyasi nima?
+Funktsiya deklaratsiyasi funktsiyani belgilaydigan identifikatorni kiritadi va ixtiyoriy ravishda funktsiya parametrlarining turlarini (prototipi) belgilaydi. Funktsiya e'lonlari (ta'riflardan farqli o'laroq) fayllar ko'lami bilan bir qatorda blok doirasida ham paydo bo'lishi mumkin.*
+
+- *Funktsiya declaration qolganlaridan  yanabir kuchli farqi uni elon qilib olganimazdan so'ng istalgan joyda chaqirib ishlata olamiz hatto funksiya elongqilingan satirdan yuqorida chaqirib ham !*
+### *Function expession*
+### *Arrow function*
