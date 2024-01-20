1671a44916bcb050f7bf89bf38705dff4fd112c2
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-01-20 20:50:33 +0300

    lesson-9 array metod redme

--- lesson-9/all.js
+++ lesson-9/all.js
@@ -14,8 +14,23 @@
 // let c ="n";
 
 // console.log(s.replace(c , c+c));
+//--------------------------------------------
+// let numbers = [1, 2, 3, 4 , 2 , 2];
+// let sum = numbers.reduce((a, b) => a * b, );
+// console.log(sum); // 10
 
-let numbers = [1, 2, 3, 4 , 2 , 2];
-let sum = numbers.reduce((a, b) => a * b, );
-console.log(sum); // 10
+//-----------------------------------------------------
 
+// find()
+
+// const result = [1, 4, 2, 3, 7, 9 ];
+// const findValue = (n) => n === 2 ;
+// const findValue2= result.find(findValue);
+// console.log(findValue2);
+
+//-------------------------------------------------------
+// splick()
+
+let fruits = ["apple", "banana", "orange", "grape"];
+fruits.splice(2, 1, "kiwi", "melon");
+console.log(fruits); // ["apple", "banana", "kiwi", "melon", "grape"]
