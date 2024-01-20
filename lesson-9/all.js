10fc3d0a0c1c2a902a051610587deebf2fde755f
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-01-20 11:53:55 +0300

    lesson-9

---
+++ lesson-9/all.js
@@ -0,0 +1,21 @@
+//- misol 
+
+// let a = prompt("O'zingiz haqingizda malumot kiriting : ");
+
+// a = a.split(" ").length;
+// console.log(a);
+
+//-----------------------------------------
+//  let a = 'nimadur';
+//  console.log(a.split("").reverse().join(""));
+//===================================
+
+// let s = "nimadur";
+// let c ="n";
+
+// console.log(s.replace(c , c+c));
+
+let numbers = [1, 2, 3, 4 , 2 , 2];
+let sum = numbers.reduce((a, b) => a * b, );
+console.log(sum); // 10
+
