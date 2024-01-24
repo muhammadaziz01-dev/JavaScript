e4c6a0ffbe4f6b76882e56cbf2e0425d4cf05898
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-01-24 18:20:11 +0300

    lesson-12

--- lesson-12/app.js
+++ lesson-12/app.js
@@ -10,3 +10,85 @@
 
 // 2 - misol
 
+// const createCounter = (n) =>{
+//     return n++;
+// }
+
+// console.log(createCounter(6));
+
+//-------------------------------------------------
+
+// 3 - misol
+
+// let praducts = [
+//     {
+//         name : "Apelsin",
+//         price : 10000,
+       
+//     },
+//     {
+//         name : "Olma",
+//         price : 12000,
+//         wight : 1,
+//     },
+//     {
+//         name : "Mandalin",
+//         price : 8000,
+//     },
+//     {
+//         name : "Banan",
+//         price : 20000,
+//     },
+// ];
+// let summa = 0;
+// for(let i = 0; i<praducts.length ; i++ ){
+//     summa += praducts[i].price;
+// }
+// console.log(summa);
+
+//-----------------------------------------------------------
+
+// 4 - misol
+
+// const books = [
+//     {
+//       title: "Halqa",
+//       author: "Akrom Malik",
+//       alreadyReafunction: false,
+//     },
+//     {
+//       title: "Dunyo ishlari",
+//       author: "O'tkir Hoshimov",
+//       alreadyRead: true,
+//     },
+//     {
+//       title: "Vaqtning qadri",
+//       author: "Abdulfattoh Abu G'udda",
+//       alreadyRead: false,
+//     },
+// ];
+// let book = "";
+// let after ="";
+// for(let i = 0 ; i<=books.length; i++){
+//     if(books[i].alreadyRead){
+//         console.log(`${books[i].author} ning "${books[i].title}" kitobi o'qilgan;`);
+//     }else{
+//         console.log(`${books[i].author} ning "${books[i].title}" kitobi o'qilmagan;`);
+//     }
+// }
+
+//---------------------------------------------------------------------------------------------
+
+// 5- misol
+
+ const obj = {
+    "it": 20,
+    "mushuk": 10,
+    "sigir": 200,
+    "tovuq": 2
+};
+
+const obj2 =[];
+
+console.log(obj2);
+  
